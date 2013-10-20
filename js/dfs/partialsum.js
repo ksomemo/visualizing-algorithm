e7e531f2fb0b314c45d87bb55fedef30d0ff31af
k_somemo <kntsmy@hotmail.co.jp>
2013-10-20 20:33:42 +0900

    テストケース2の動作をJavascriptで行うようにした。

--- js/dfs/partialsum.js
+++ js/dfs/partialsum.js
@@ -3,10 +3,20 @@ var numCntDom = document.getElementById("numCnt");
 var sumDom = document.getElementById("sum");
 var numsDom = document.getElementById("nums");
 var testcase1 = document.getElementById("testcase1");
+var testcase2 = document.getElementById("testcase2");
 
 testcase1.onclick = function () {
 	var nums = [1, 2, 4, 7];
-	var sum = 0;
+	var sum = 13;
+
+	numCntDom.innerHTML = "要素数:" + nums.length;
+	numsDom.innerHTML = "要素:" + nums;
+	sumDom.innerHTML = "合計:" + sum;
+};
+
+testcase2.onclick = function () {
+	var nums = [1, 2, 4, 7];
+	var sum = 15;
 
 	numCntDom.innerHTML = "要素数:" + nums.length;
 	numsDom.innerHTML = "要素:" + nums;
