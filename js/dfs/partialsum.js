69f4e2fc94f73aff783ed9a8ea1d9ff6c8fb74b3
k_somemo <kntsmy@hotmail.co.jp>
2013-10-20 20:38:54 +0900

    部分和を求める関数を作成し、クリックされた時の動作を委譲した。

--- js/dfs/partialsum.js
+++ js/dfs/partialsum.js
@@ -5,21 +5,25 @@ var numsDom = document.getElementById("nums");
 var testcase1 = document.getElementById("testcase1");
 var testcase2 = document.getElementById("testcase2");
 
-testcase1.onclick = function () {
-	var nums = [1, 2, 4, 7];
-	var sum = 13;
+var partialSum = function(testcase) {
 
-	numCntDom.innerHTML = "要素数:" + nums.length;
-	numsDom.innerHTML = "要素:" + nums;
-	sumDom.innerHTML = "合計:" + sum;
+	numCntDom.innerHTML = "要素数:" + testcase.nums.length;
+	numsDom.innerHTML = "要素:" + testcase.nums;
+	sumDom.innerHTML = "合計:" + testcase.sum;
+
+};
+
+testcase1.onclick = function () {
+	partialSum({
+		nums: [1, 2, 4, 7],
+		sum: 13
+	});
 };
 
 testcase2.onclick = function () {
-	var nums = [1, 2, 4, 7];
-	var sum = 15;
-
-	numCntDom.innerHTML = "要素数:" + nums.length;
-	numsDom.innerHTML = "要素:" + nums;
-	sumDom.innerHTML = "合計:" + sum;
+	partialSum({
+		nums: [1, 2, 4, 7],
+		sum: 15
+	});
 };
 
