4f3950984445683a1d02dfde9e48cc668be0d7ef
k_somemo <kntsmy@hotmail.co.jp>
2013-11-19 05:53:20 +0900

    実行するアルゴリズムを選択できるようにした。

--- knapsack/dp.js
+++ knapsack/dp.js
@@ -56,14 +56,33 @@ var defaultColor = 'white';
 var posDom       = null;
 var reusePosDom  = null;
 var befGoodsId   = -1;
+var loopNum      = 1;
+var interval     = 1000;
 
-var loopNum = 1;
-var interval = 1000;
-for (i = 0; i < num; i++) {
-    for (j = 0; j <= limit; j++) {
-        setTimeout((function (i, j, loop) {
+var startBtn = document.getElementById('start');
+startBtn.onclick = function () {
+    if (startBtn.disabled) return;
+    startBtn.disabled = true;
+
+    var radios = document.getElementsByName('knapsack');
+    var type = "1";
+    for (i = 0; i < radios.length; i++) {
+        if (radios[i].checked) type = radios[i].value;
+    }
+    var func = (type === "1") ? knapsack : optimizedKnapsack;
+
+    for (i = 0; i < num; i++) {
+        for (j = 0; j <= limit; j++) {
+            func();
+        }
+    }
+};
+
+function knapsack() {
+    for (k = 0; k * goods[i].w <= j; k++) {
+        setTimeout((function (i, j, k, loop) {
             return function () {
-                console.log('Loop[' + loop + ']i,j:' + i +',' + j);
+                console.log('Loop[' + loop + ']i,j,k:' + i +',' + j + ',' + k);
 
                 if (befGoodsId !== i) {
                     var befGood = document.getElementById(goodsIdName(befGoodsId));
@@ -75,74 +94,72 @@ for (i = 0; i < num; i++) {
                 if (posDom)     posDom.style.backgroundColor      = defaultColor;
                 if (reusePosDom)reusePosDom.style.backgroundColor = defaultColor;
 
-                var nowPos    = i + 1;
-                var reusePos  = j - goods[i].w;
-                var reusable  = reusePos >= 0;
-                var befV      = dp[i][j];
-                var reuseV    = replasable ? dp[i + 1][reusePos] : befV;
-                var newV      = replasable ? reuseV + goods[i].v : befV;
+                var befPos    = i + 1;
+                var reusePos  = j - k * goods[i].w;
+                var reuseV    = dp[i][reusePos];
+                var befV      = dp[befPos][j];
+                var newV      = reuseV + k * goods[i].v;
 
-                posDom = document.getElementById(valIdName(nowPos, j));
+                posDom      = document.getElementById(valIdName(befPos, j));
+                reusePosDom = document.getElementById(valIdName(i, reusePos));
                 posDom.style.backgroundColor      = 'pink';
-                if (replasable) {
-                    reusePosDom = document.getElementById(valIdName(i + 1, reusePos));
-                    reusePosDom.style.backgroundColor = 'skyblue';
-                } else {
-                    reusePosDom = document.getElementById(valIdName(i, j));
-                    reusePosDom.style.backgroundColor = 'gray';
-                }
+                reusePosDom.style.backgroundColor = 'skyblue';
 
                 befValDom.innerHTML    = befV;
-                newValDom.innerHTML    = !replasable ? '入れ替えられない' : newV + '(' + reuseV + ' + '+ goods[i].v + ')';
-                addTotalWDom.innerHTML = !replasable ? '入れ替えられない' : goods[i].w : ;
+                newValDom.innerHTML    = newV + '(' + reuseV + ' + '+ k + ' * ' + goods[i].v + ')';
+                addTotalWDom.innerHTML = k * goods[i].w + '(' + k + ' * ' + goods[i].w + ')';
 
-                if (replasable) {
-                    dp[nowPos][j] = Math.max(befV, newV);
-                } else {
-                    dp[nowPos][j] = dp[i][j];
-                }
+                dp[befPos][j] = Math.max(befV, newV);
 
-                document.getElementById(valIdName(nowPos, j)).innerHTML = dp[nowPos][j];
+                document.getElementById(valIdName(befPos, j)).innerHTML = dp[i + 1][j];
             };
-        }(i, j, loopNum)), (loopNum++) * interval);
-/*
-        for(k = 0; k * goods[i].w <= j; k++) {
-            setTimeout((function (i, j, k, loop) {
-                return function () {
-                    console.log('Loop[' + loop + ']i,j,k:' + i +',' + j + ',' + k);
-
-                    if (befGoodsId !== i) {
-                        var befGood = document.getElementById(goodsIdName(befGoodsId));
-                        if (befGood) befGood.style.backgroundColor = defaultColor;
-                        befGoodsId = i;
-                        document.getElementById(goodsIdName(i)).style.backgroundColor = 'yellow';
-                    }
-
-                    if (posDom)     posDom.style.backgroundColor      = defaultColor;
-                    if (reusePosDom)reusePosDom.style.backgroundColor = defaultColor;
-
-                    var befPos    = i + 1;
-                    var reusePos  = j - k * goods[i].w;
-                    var reuseV    = dp[i][reusePos];
-                    var befV      = dp[befPos][j];
-                    var newV      = reuseV + k * goods[i].v;
-
-                    posDom      = document.getElementById(valIdName(befPos, j));
-                    reusePosDom = document.getElementById(valIdName(i, reusePos));
-                    posDom.style.backgroundColor      = 'pink';
-                    reusePosDom.style.backgroundColor = 'skyblue';
-
-                    befValDom.innerHTML    = befV;
-                    newValDom.innerHTML    = newV + '(' + reuseV + ' + '+ k + ' * ' + goods[i].v + ')';
-                    addTotalWDom.innerHTML = k * goods[i].w + '(' + k + ' * ' + goods[i].w + ')';
-
-                    dp[befPos][j] = Math.max(befV, newV);
-
-                    document.getElementById(valIdName(befPos, j)).innerHTML = dp[i + 1][j];
-                };
-            })(i, j, k, loopNum), (loopNum++) * interval);
-        }
-*/
+        })(i, j, k, loopNum), (loopNum++) * interval);
     }
 }
 
+function optimizedKnapsack() {
+    setTimeout((function (i, j, loop) {
+        return function () {
+            console.log('Loop[' + loop + ']i,j:' + i +',' + j);
+
+            if (befGoodsId !== i) {
+                var befGood = document.getElementById(goodsIdName(befGoodsId));
+                if (befGood) befGood.style.backgroundColor = defaultColor;
+                befGoodsId = i;
+                document.getElementById(goodsIdName(i)).style.backgroundColor = 'yellow';
+            }
+
+            if (posDom)     posDom.style.backgroundColor      = defaultColor;
+            if (reusePosDom)reusePosDom.style.backgroundColor = defaultColor;
+
+            var nowPos    = i + 1;
+            var reusePos  = j - goods[i].w;
+            var reusable  = reusePos >= 0;
+            var befV      = dp[i][j];
+            var reuseV    = reusable ? dp[i + 1][reusePos] : befV;
+            var newV      = reusable ? reuseV + goods[i].v : befV;
+
+            posDom = document.getElementById(valIdName(nowPos, j));
+            posDom.style.backgroundColor      = 'pink';
+            if (reusable) {
+                reusePosDom = document.getElementById(valIdName(i + 1, reusePos));
+                reusePosDom.style.backgroundColor = 'skyblue';
+            } else {
+                reusePosDom = document.getElementById(valIdName(i, j));
+                reusePosDom.style.backgroundColor = 'gray';
+            }
+
+            befValDom.innerHTML    = befV;
+            newValDom.innerHTML    = !reusable ? '入れ替えられない' : newV + '(' + reuseV + ' + '+ goods[i].v + ')';
+            addTotalWDom.innerHTML = !reusable ? '入れ替えられない' : goods[i].w;
+
+            if (reusable) {
+                dp[nowPos][j] = Math.max(befV, newV);
+            } else {
+                dp[nowPos][j] = dp[i][j];
+            }
+
+            document.getElementById(valIdName(nowPos, j)).innerHTML = dp[nowPos][j];
+        };
+    })(i, j, loopNum), (loopNum++) * interval);
+}
